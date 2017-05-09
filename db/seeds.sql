d685e59a80dc2ebf5eef3306f46e2a1b525158d7
Emily Keen <emily.keen711@gmail.com>
2017-05-09 09:44:52 -0400

    last weeks homework

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Mac and Cheese Burger', false, '2017-04-30 11:50:20');
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Veggie Burger', false, '2017-04-30 11:55:20');
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Bacon Burger', false, '2017-05-30 11:55:20');
+select * from burgers;
+
